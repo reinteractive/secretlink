661bfce25e5dd8ecde64ce0a50889f21fbdd61e6
Raphael Campardou <ralovely@gmail.com>
2015-04-29 09:04:36 +1000

    Add deploy hooks

---
+++ config/deploy/hooks/lib/sidekiq.sh
@@ -0,0 +1,27 @@
+sidekiq_init_script="/etc/init.d/sidekiq-${app_full_name}"
+
+service_sidekiq(){
+  if [ -f $sidekiq_init_script ] ; then
+    echo "executing ${sidekiq_init_script} $1"
+    $sidekiq_init_script $1
+  else
+    echo "Sidekiq is not installed. Skipping $1"
+  fi
+}
+
+stop_sidekiq(){
+  service_sidekiq stop
+}
+
+start_sidekiq(){
+  service_sidekiq start
+}
+
+restart_sidekiq(){
+  service_sidekiq stop
+  service_sidekiq start
+}
+
+soft_stop_sidekiq(){
+  service_sidekiq soft_stop
+}
