f7f78585e1270ba8fc3658eeda305b7b35fcda7a
Raphael Campardou <ralovely@gmail.com>
2015-04-29 10:00:26 +1000

    Remove the from address in order to use the default one

--- app/mailers/auth_token_mailer.rb
+++ app/mailers/auth_token_mailer.rb
@@ -4,7 +4,7 @@ class AuthTokenMailer < ActionMailer::Base
     @email = email
     @request_host = request_host
     @token = token
-    mail(to: email, from: 'noreply@snapsecret.com', subject: 'SnapSecret authentication token')
+    mail(to: email, subject: 'SnapSecret authentication token')
   end
 
 end
