16a35294d2cfb90d6448fb30ee2d64bc487e2856
Mikel Lindsaar <mikel@reinteractive.net>
2015-06-21 10:13:28 +1000

    Adding callback URL as an ENV VAR

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :secrets, only: [:show, :new, :create]
   resources :decrypted_secrets, only: :create
   post '/decrypt_secret', to: 'decrypted_secrets#create'
-  get '/auth/google_oauth2/callback', to: 'oauth_callbacks#google'
+  get ENV['GOOGLE_OAUTH_CALLBACK_PATH'], to: 'oauth_callbacks#google'
   get '/auth/failure', to: 'oauth_callbacks#auth_failure'
   get '/:uuid/:secret_key/:auth_token', to: 'secrets#show'
   root 'auth_tokens#new'
