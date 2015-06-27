8949710d193cd6e0070425758fa2b371ca50489c
Julia <julia.ryanx@gmail.com>
2015-06-27 13:09:23 +1000

    disable multiple form submits

--- app/assets/javascripts/topsekrit.js
+++ app/assets/javascripts/topsekrit.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
   $('.datepicker').pickadate();
+  $('form').on('submit', function(e){
+    $(e.target).find('input[type="submit"]').prop('disabled', true);
+  });
 });
