05359d91bd8d97768dc156a2fc47fdb86982360c
Mikel Lindsaar <mikel@reinteractive.net>
2017-04-10 17:26:13 +1000

    Fixing issue with Google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
 Rails.application.config.after_initialize do
   OmniAuth.config.full_host = Rails.configuration.topsekrit_base_url
 end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-           Rails.application.config.topsekrit_google_oauth_client_id,
-           Rails.application.config.topsekrit_google_oauth_client_secret
+           Rails.configuration.topsekrit_google_oauth_client_id,
+           Rails.configuration.topsekrit_google_oauth_client_secret
 end
+
