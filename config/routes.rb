98f4ed17c65196dc19d1863910e87794ef422f59
wetoolaguer <wetoolaguer@gmail.com>
2019-06-25 13:36:36 +0800

    Removes unneeded auth_tokens_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
     passwords: 'users/passwords'
   }
 
-  resources :auth_tokens, only: [:show, :new, :create]
   resources :secrets, only: [:show, :new, :create, :edit, :update]
   resources :decrypted_secrets, only: :create
 
@@ -13,9 +14,6 @@ Rails.application.routes.draw do
   get Rails.application.config.topsekrit_google_oauth_callback_path, to: 'oauth_callbacks#google'
 
   get '/auth/failure', to: 'oauth_callbacks#auth_failure'
-
-  get '/auth/new', to: 'auth_tokens#new'
-  root 'pages#home'
 
   get '/copyright',            to: 'pages#copyright'
   get '/privacy_policy',       to: 'pages#privacy_policy'
