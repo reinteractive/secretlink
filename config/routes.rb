a42d77b4a90b1db008fdef2d09ccc5f61701aad6
wetoolaguer <wetoolaguer@gmail.com>
2019-06-26 08:43:58 +0800

    Adds recaptcha validation in registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    registrations: 'users/registrations',
     confirmations: 'users/confirmations',
     passwords: 'users/passwords'
   }
