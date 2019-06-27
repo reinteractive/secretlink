d80ee39817119a415b4115233c93cbe186f00b61
wetoolaguer <wetoolaguer@gmail.com>
2019-06-27 17:48:10 +0800

    Redirects to new_secret_path when user has no secret

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < AuthenticatedController
   def index
-    @secrets = current_user.secrets.order(created_at: :desc).last(10)
+    @secrets = current_user.secrets.order(created_at: :desc).first(10)
+
+    if @secrets.empty?
+      flash[:notice] = t('welcome')
+      redirect_to new_secret_path
+    end
   end
 end
