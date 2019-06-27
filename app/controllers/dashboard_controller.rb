e4a5e44412029b558a06d180ac2adbfe0d310f55
wetoolaguer <wetoolaguer@gmail.com>
2019-06-27 12:51:19 +0800

    Styles secret-item

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < AuthenticatedController
   def index
-    @secrets = current_user.secrets
+    @secrets = current_user.secrets.order(created_at: :desc).last(10)
   end
 end
