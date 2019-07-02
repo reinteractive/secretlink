7ba6841f4a385330ff3c266db03711b0a55a1f15
wetoolaguer <wetoolaguer@gmail.com>
2019-07-02 16:59:31 +0800

    Adds TwoFactorService

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :two_factor_authenticatable,
-         otp_secret_encryption_key: Rails.configuration.topsekrit_2fa_key
+          :recoverable, :rememberable, :trackable, :validatable
+
+  devise :two_factor_authenticatable,
+          otp_secret_encryption_key: Rails.configuration.topsekrit_2fa_key
 
   validate :email_authorised?, on: :create
 
---
+++ app/services/two_factor_service.rb
@@ -0,0 +1,42 @@
+class TwoFactorService
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+  end
+
+  def issue_otp_secret
+    user.otp_secret = User.generate_otp_secret
+  end
+
+  def generate_otp_provisioning_uri
+    issuer = 'Secret Link'
+    issuer += ' (dev)' if Rails.env.development?
+    label = "#{issuer}:#{user.email}"
+    user.otp_provisioning_uri(label, issuer: issuer)
+  end
+
+  # Update model with params only if otp_attempt and current_password are correct
+  def enable_otp(otp_secret, otp_attempt, current_password)
+    user.otp_required_for_login = true
+    otp_valid = user.validate_and_consume_otp!(otp_attempt, otp_secret: otp_secret)
+    password_valid = user.valid_password?(current_password)
+
+    result = if otp_valid && password_valid
+      user.update_attributes(otp_secret: otp_secret, otp_required_for_login: true)
+    else
+      user.assign_attributes(otp_secret: otp_secret)
+      user.valid?
+      user.errors.add(:otp_attempt, otp_attempt.blank? ? :blank : :invalid) if !otp_valid
+      user.errors.add(:current_password, current_password.blank? ? :blank : :invalid) if !password_valid
+      false
+    end
+
+    user.clean_up_passwords
+    result
+  end
+
+  def disable_otp(current_password)
+    user.update_with_password(current_password: current_password, otp_required_for_login: false)
+  end
+end
