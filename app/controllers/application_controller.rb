4fa690b18769cd33fd06c0451fe0770232c27387
wetoolaguer <wetoolaguer@gmail.com>
2019-07-03 15:13:15 +0800

    Enables logging in with OTP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protect_from_forgery with: :exception
   layout :layout_by_resource
 
@@ -18,5 +20,9 @@ class ApplicationController < ActionController::Base
     else
       "application"
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
   end
 end
