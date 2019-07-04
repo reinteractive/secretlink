6a2015b5f8e5759b57954f74d603d46d16d336b2
wetoolaguer <wetoolaguer@gmail.com>
2019-07-04 17:50:55 +0800

    Adds two-factor fields in user setup

--- app/controllers/user_setups_controller.rb
+++ app/controllers/user_setups_controller.rb
@@ -1,5 +1,8 @@
 class UserSetupsController < AuthenticatedController
   def edit
+    @tfa_service = TwoFactorService.new(current_user)
+    @tfa_service.issue_otp_secret
+    @otp_provisioning_uri = @tfa_service.generate_otp_provisioning_uri
   end
 
   def update
