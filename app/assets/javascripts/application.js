2a1a69a9c36b6ae631f4be4061b9d0511932dd8f
wetoolaguer <wetoolaguer@gmail.com>
2019-07-04 14:24:08 +0800

    Enables ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery
+//= require jquery_ujs
 //= require pickadate/picker
 //= require pickadate/picker.date
 //= require bootstrap-sprockets
