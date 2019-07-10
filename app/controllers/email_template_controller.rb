5373dbf1f9485b012f9743b326a863a19a093268
wetoolaguer <wetoolaguer@gmail.com>
2019-07-10 13:28:06 +0800

    Overrides send_secret_email_template when nil

--- app/controllers/email_template_controller.rb
+++ app/controllers/email_template_controller.rb
@@ -3,8 +3,18 @@ class EmailTemplateController < AuthenticatedController
 
   def edit
     @settings = current_user.settings
+
+    unless @settings.send_secret_email_template
+      override = ERB.new(load_template).result(view_context.__binding__)
+      @settings.send_secret_email_template = override
+    end
   end
 
   def update
+  end
+
+  def load_template
+    path = Rails.root.join('app/views/secret_mailer/secret_notification_editable.html.erb')
+    File.read(path)
   end
 end
